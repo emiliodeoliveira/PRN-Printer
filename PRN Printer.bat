9d190ccc1990db68d69310ed544e004c28a8609d
Emilio de Oliveira <emilio.oliveira@safelight.info>
2018-01-03 16:59:55 -0200

    Adicionada a versão 0.4.

--- PRN Printer.bat
+++ PRN Printer.bat
@@ -1,7 +1,7 @@
 @echo off
 title PRN Printer - v0.3
-REM Ferramenta para impressão de arquivos PRN. Para atualizações, acesse o repositório no GitHub.
-REM Repositório: https://github.com/emiliodeoliveira/PRN-Printer
+REM Ferramenta para impressÃ£o de arquivos PRN. Para atualizaÃ§Ãµes, acesse o repositÃ³rio no GitHub.
+REM RepositÃ³rio: https://github.com/emiliodeoliveira/PRN-Printer
 REM E-mail: emilio.oliveira@safelight.info
 REM Em caso de erros, favor contatar no e-mail acima.
 cd database
@@ -63,14 +63,14 @@ title PRN Printer\Menu\Sair
 cls
 echo.
 set sair=0
-set /p sair=Vocˆ tem certeza que deseja sair?(s/n):
+set /p sair=VocË† tem certeza que deseja sair?(s/n):
 if %sair% equ 0 goto SAIR
 if %sair% equ S exit -s
 if %sair% equ s exit -s
 if %sair% equ N goto MENU
 if %sair% equ n goto MENU
 echo.
-echo Comando inv lido! Digite a tecla "S" ou a tecla "s" para "Sim" e a tecla "N" ou a tecla "n" para "NÆo".
+echo Comando inv lido! Digite a tecla "S" ou a tecla "s" para "Sim" e a tecla "N" ou a tecla "n" para "NÃ†o".
 pause>nul
 goto SAIR
 
